2b6cba6b60de2bc0ea4111c58970be66eb888ccd
hongyu <lhy429581492@gmail.com>
2024-01-24 13:55:06 +0800

    feat:http cache

--- test/test.go
+++ test/test.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"github.com/Godyu97/geecache/gee"
 )
 
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
 func main() {
-	fmt.Println("gee cache~")
+	gee.NewGroup("scores", 2<<10, gee.GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	addr := "0.0.0.0:9999"
+	peers := gee.NewHTTPPool(addr)
+	log.Println("geecache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr, peers))
 }
